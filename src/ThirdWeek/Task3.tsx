0d8b4c51eb8992af33f0babe017f3e9c878b699d
mr_darcy <mr_darcy_1@mail.ru>
2022-11-10 02:37:01 +0500

    Commit1

--- src/ThirdWeek/Task3.tsx
+++ src/ThirdWeek/Task3.tsx
@@ -1,22 +1,46 @@
 import React, {useState} from 'react';
 import Todolist3 from "./Todolist3";
+import {v1} from "uuid";
+
+export type FilteredType = 'all' | 'active' | 'completed'
 
 const Task3 = () => {
     const [tasks1,setTasks] =  useState([
-        {id:1, name:'HTML&CSS', priority:true},
-        {id:2, name:'JS', priority:false},
-        {id:3, name:'ReactJS', priority:true},
-        {id:4, name:'Rest API', priority:false},
-        {id:5, name:'GraphQL', priority:true}
+        {id:v1(), name:'HTML&CSS', priority:true},
+        {id:v1(), name:'JS', priority:false},
+        {id:v1(), name:'ReactJS', priority:true},
+        {id:v1(), name:'Rest API', priority:false},
+        {id:v1(), name:'GraphQL', priority:true}
     ])
 
-    function removeTask(id:number){
+    function removeTask(id:string){
         let filteredTasks = tasks1.filter(t=>t.id !== id)
         setTasks(filteredTasks)
     }
+
+    let [filter,setFilter] = useState<FilteredType>("all")
+
+    let tasksForTodolist = tasks1;
+
+    if (filter==='active') {
+        tasksForTodolist = tasks1.filter(t => t.priority)
+        // tasksForTodolist = tasks1.filter(t => t.priority)
+    }
+    if (filter==='completed') {
+        tasksForTodolist = tasks1.filter(t => !t.priority)
+    }
+
+    function changeFilter(value:FilteredType){
+        setFilter(value)
+    }
+
     return (
         <div>
-            <Todolist3 title={'Что я знаю?'} tasks={tasks1} delete={removeTask}/>
+            <Todolist3 title={'Что я знаю?'}
+                       tasks={tasksForTodolist}
+                       delete={removeTask}
+                       changeFilter={changeFilter}
+            />
         </div>
     );
 };
--- src/ThirdWeek/Todolist3.tsx
+++ src/ThirdWeek/Todolist3.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import {FilteredType} from "./Task3";
 
 type TasksPropsType = {
     title:string
     tasks:TaskType[]
-    delete:(id:number)=>void
+    delete:(id:string)=>void
+    changeFilter:(value:FilteredType)=>void
 }
 type TaskType = {
-    id:number,
+    id:string,
     name:string,
     priority:boolean
 }
@@ -14,15 +16,18 @@ type TaskType = {
 const Todolist3 = (props:TasksPropsType) => {
     return (
         <div>
-            <h3>{props.title}</h3>
+            <h3>{ props.title}</h3>
             <input/>
             <button>+</button>
-            <ul>{props.tasks.map(el => <li key={el.id}>
+            <ul>{ props.tasks.map(el => <li key={el.id}>
                 <input type="checkbox" checked={el.priority}/>
                 {el.name}
                 <button onClick={()=>{props.delete(el.id)}}>x</button>
             </li> )}
             </ul>
+            <button onClick={()=>{props.changeFilter('all')}}>all</button>
+            <button onClick={()=>{props.changeFilter('active')}}>active</button>
+            <button onClick={()=>{props.changeFilter('completed')}}>completed</button>
         </div>
     );
 };
