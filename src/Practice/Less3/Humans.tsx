c9a54ee8b5b2727b3c6b21247c352f042fbf558a
mr_darcy <mr_darcy_1@mail.ru>
2022-11-15 11:52:59 +0500

    Commit1

--- src/Practice/Less3/Humans.tsx
+++ src/Practice/Less3/Humans.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {FilterBestType} from "./Less3";
 
 type HumansPropsType = {
     humans:HumanType[]
     title:string
+    changeFilter:(value:FilterBestType)=>void
+    removeHuman:(id:string)=>void
 }
 type HumanType = {
     id:string,
@@ -14,14 +17,20 @@ type HumanType = {
  export const Humans = (props:HumansPropsType) => {
     return (
         <div>
-            {props.humans.map(h=>{
-                return (
-                    <ul key={h.id}>
-                        <input type={"checkbox"} checked={h.isDone}/>
-                        <span>{h.name}</span>
-                    </ul>
-                )
-            })}
+            <h1>{props.title}</h1>
+            <input/>
+            <ul>
+                {
+                    props.humans.map(t => <li key={t.id}>
+                        <input type="checkbox" checked={t.isDone}/>
+                        <span>{t.name}</span>
+                        <button onClick={ () => { props.removeHuman(t.id) } }>x</button>
+                    </li>)
+                }
+            </ul>
+            <button onClick={()=>{props.changeFilter("all")}}>All</button>
+            <button onClick={()=>{props.changeFilter("wonder")}}>The Wonder</button>
+            <button onClick={()=>{props.changeFilter('the best')}}>The Best</button>
         </div>
     );
 };
