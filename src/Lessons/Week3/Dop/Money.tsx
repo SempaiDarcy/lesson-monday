3c332fab75f503c6100f549fde0946f090f87017
mr_darcy <mr_darcy_1@mail.ru>
2022-11-19 18:25:48 +0500

    Commit1

--- src/Lessons/Week3/Dop/Money.tsx
+++ src/Lessons/Week3/Dop/Money.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 
+export type BanknotesType = 'All'|'Dollar' | 'Tenge' | 'RUBLS'
+
+export type MoneyType = {
+    banknotes:BanknotesType,
+    number:number,
+    value:string
+}
+
 const Money = () => {
+
+
+    let defaultMoney: MoneyType[] = [
+        {banknotes:'Dollar', number:250,value:'a12345678'},
+        {banknotes:'Dollar', number:500,value:'b12345678'},
+        {banknotes:'Tenge', number:250,value:'c12345678'},
+        {banknotes:'Dollar', number:100,value:'d12345678'},
+        {banknotes:'Tenge', number:250,value:'e12345678'},
+        {banknotes:'Dollar', number:1000,value:'f12345678'},
+        {banknotes:'RUBLS', number:100,value:'g12345678'},
+        {banknotes:'Dollar', number:150,value:'h12345678'},
+        {banknotes:'RUBLS', number:50,value:'k12345678'},
+    ]
+
     return (
         <div>
 
