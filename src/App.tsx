6df3266a2f200f9d9f82d2cd61acca46a20630ca
mr_darcy <mr_darcy_1@mail.ru>
2022-12-04 04:04:44 +0500

    Commit100

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Step01 from "./Lessons/Monday/Todolists/Step-01";
+import Step01 from "./Lessons/Monday/Todolists/Todolist-01/Step-01";
 
 function App() {
     return (
