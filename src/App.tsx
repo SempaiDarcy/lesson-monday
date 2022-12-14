a33cb737e76cd33aa1e0aa9a50322aa91eb2fce7
SempaiDarcy <mr_darcy_1@mail.ru>
2022-12-14 13:03:07 +0500

    LM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Components from "./Lessons/Monday/React-Kabzda-Kak-Podrobno/Week1/Components";
+import Dop5 from "./Lessons/Sunday/Todolists/Dop/Dop5";
 
 function App() {
     return (
         <div className="App">
             <Components/>
+            <Dop5/>
         </div>
     );
 }
